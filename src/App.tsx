39a0fcf2cf726191069927397afa81ef95774d1b
heywon0909 <hw731911@gmail.com>
2024-10-05 19:46:27 +0900

    feat: react query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import ListContainer from "./components/list/ListContainer";
 
 
 function App() {
-  
+  const queryClient = new QueryClient()
   return (
+    <QueryClientProvider client={queryClient}>
     <div className="App">
       <header className="App-header">
        Your fleet
       </header>
       <ListContainer/>
-    </div>
+      </div>
+    </QueryClientProvider>
   );
 }
 
